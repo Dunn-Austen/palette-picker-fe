36e2f19bf2bc82cb12a677b4c9edbdfe61891925
Alan Turing <austeyd@gmail.com>
2020-02-08 16:28:14 -0700

    Add initial code for ColorCard

---
+++ src/ColorCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './ColorCard.css'
+
+const ColorCard = props => {
+  const { hexColor } = props;
+
+  return (
+    <article className='color-card' style={{backgroundColor: `#${hexColor}`}}>
+      <h1 className='hex-display'>
+        {hexColor}
+      </h1>
+      <button>
+        Freeze
+      </button>
+    </article>
+  );
+}
+
+
+//Button requires onClick{() => } functionality for freezing color choice
