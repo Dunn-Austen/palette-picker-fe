4596ee570508d04fc43ad976bd56f222ba3ffc71
Alan Turing <austeyd@gmail.com>
2020-02-09 10:40:00 -0700

    Set up map functionality for displayColorCards

--- src/ColorContainer.js
+++ src/ColorContainer.js
@@ -3,17 +3,17 @@ import ColorCard from './ColorCard';
 import './ColorContainer.css';
 
 const ColorContainer = ({paletteColors, isSavedStatuses, setIsSavedFunctions}) => {
-  const {paletteColor_1, paletteColor_2, paletteColor_3, paletteColor_4, paletteColor_5} = paletteColors;
-  const {isSaved_1, isSaved_2, isSaved_3, isSaved_4, isSaved_5} = isSavedStatuses;
-  const {setIsSaved_1, setIsSaved_2, setIsSaved_3, setIsSaved_4, setIsSaved_5} = setIsSavedFunctions;
+  let {paletteColor_1, paletteColor_2, paletteColor_3, paletteColor_4, paletteColor_5} = paletteColors;
+  let {isSaved_1, isSaved_2, isSaved_3, isSaved_4, isSaved_5} = isSavedStatuses;
+  let {setIsSaved_1, setIsSaved_2, setIsSaved_3, setIsSaved_4, setIsSaved_5} = setIsSavedFunctions;
+  let paletteColors = [paletteColor_1, paletteColor_2, paletteColor_3, paletteColor_4, paletteColor_5];
 
-  const displayColorCards = colors.map((color, index) => {
+  const displayColorCards = paletteColors.map((color, index) => {
     return (
       <ColorCard
         hexCode={color}
         key={color}
-      >
-      </div>
+      />
     );
   });
 
