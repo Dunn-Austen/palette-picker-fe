9119121e5a3a21ca0b7c9ca4b0205deaadd8e363
Alan Turing <austeyd@gmail.com>
2020-02-09 10:34:08 -0700

    Adjust frozenStatus to isSaved

--- src/App.js
+++ src/App.js
@@ -11,30 +11,28 @@ const App = () => {
   let [palleteColor_4, setPalleteColor_4] = useState('');
   let [palleteColor_5, setPalleteColor_5] = useState('');
 
-  let [frozenStatus_1, setFrozenStatus_1] = useState(false);
-  let [frozenStatus_2, setFrozenStatus_2] = useState(false);
-  let [frozenStatus_3, setFrozenStatus_3] = useState(false);
-  let [frozenStatus_4, setFrozenStatus_4] = useState(false);
-  let [frozenStatus_5, setFrozenStatus_5] = useState(false);
+  let [isSaved_1, setIsSaved_1] = useState(false);
+  let [isSaved_2, setIsSaved_2] = useState(false);
+  let [isSaved_3, setIsSaved_3] = useState(false);
+  let [isSaved_4, setIsSaved_4] = useState(false);
+  let [isSaved_5, setIsSaved_5] = useState(false);
 
   const generateRandomColors = () => {
-    if (!frozenStatus_1) setPaletteColor_1('#'+Math.floor(Math.random()*16777215).toString(16));
-    if (!frozenStatus_2) setPaletteColor_2('#'+Math.floor(Math.random()*16777215).toString(16));
-    if (!frozenStatus_3) setPaletteColor_3('#'+Math.floor(Math.random()*16777215).toString(16));
-    if (!frozenStatus_4) setPaletteColor_4('#'+Math.floor(Math.random()*16777215).toString(16));
-    if (!frozenStatus_5) setPaletteColor_5('#'+Math.floor(Math.random()*16777215).toString(16));
+    if (!isSaved_1) setPaletteColor_1('#'+Math.floor(Math.random()*16777215).toString(16));
+    if (!isSaved_2) setPaletteColor_2('#'+Math.floor(Math.random()*16777215).toString(16));
+    if (!isSaved_3) setPaletteColor_3('#'+Math.floor(Math.random()*16777215).toString(16));
+    if (!isSaved_4) setPaletteColor_4('#'+Math.floor(Math.random()*16777215).toString(16));
+    if (!isSaved_5) setPaletteColor_5('#'+Math.floor(Math.random()*16777215).toString(16));
   }
 
   useEffect(() => generateRandomColors(), []);
-
-
 
   return (
     <main className='app'>
       <section className='colors-section'>
         <ColorContainer
           paletteColors={paletteColor_1, paletteColor_2, paletteColor_3, paletteColor_4, paletteColor_5}
-          frozenStatuses={frozenStatus_1, frozenStatus_2, frozenStatus_3, frozenStatus_4, frozenStatus_5}
+          isSavedStatuses={isSaved_1, isSaved_2, isSaved_3, isSaved_4, isSaved_5}
         />
       </section>
     </main>
