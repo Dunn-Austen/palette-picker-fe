d7320cf66c0aba87ab1a4c9c7a84db90f7dccf0b
Alan Turing <austeyd@gmail.com>
2020-02-09 10:57:38 -0700

    Fix palette typos

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import SavePaletteForm from './SavePaletteForm';
 
 const App = () => {
 
-  let [palleteColor_1, setPaletteColor_1] = useState('');
-  let [palleteColor_2, setPalleteColor_2] = useState('');
-  let [palleteColor_3, setPalleteColor_3] = useState('');
-  let [palleteColor_4, setPalleteColor_4] = useState('');
-  let [palleteColor_5, setPalleteColor_5] = useState('');
+  let [paletteColor_1, setPaletteColor_1] = useState('#000000');
+  let [paletteColor_2, setPaleteColor_2] = useState('#bbbbbb');
+  let [paletteColor_3, setPaleteColor_3] = useState('#222222');
+  let [paletteColor_4, setPaleteColor_4] = useState('#dddddd');
+  let [paletteColor_5, setPaleteColor_5] = useState('#444444');
 
   let [isSaved_1, setIsSaved_1] = useState(false);
   let [isSaved_2, setIsSaved_2] = useState(false);
@@ -17,15 +17,25 @@ const App = () => {
   let [isSaved_4, setIsSaved_4] = useState(false);
   let [isSaved_5, setIsSaved_5] = useState(false);
 
-  const generateRandomColors = () => {
-    if (!isSaved_1) setPaletteColor_1('#'+Math.floor(Math.random()*16777215).toString(16));
-    if (!isSaved_2) setPaletteColor_2('#'+Math.floor(Math.random()*16777215).toString(16));
-    if (!isSaved_3) setPaletteColor_3('#'+Math.floor(Math.random()*16777215).toString(16));
-    if (!isSaved_4) setPaletteColor_4('#'+Math.floor(Math.random()*16777215).toString(16));
-    if (!isSaved_5) setPaletteColor_5('#'+Math.floor(Math.random()*16777215).toString(16));
-  }
-
   useEffect(() => generateRandomColors(), []);
+
+  const generateRandomColors = () => {
+    if (!isSaved_1) {
+      setPaletteColor_1('#'+Math.floor(Math.random()*16777215).toString(16))
+    };
+    if (!isSaved_2) {
+      setPaletteColor_1('#'+Math.floor(Math.random()*16777215).toString(16))
+    };
+    if (!isSaved_3) {
+      setPaletteColor_1('#'+Math.floor(Math.random()*16777215).toString(16))
+    };
+    if (!isSaved_4) {
+      setPaletteColor_1('#'+Math.floor(Math.random()*16777215).toString(16))
+    };
+    if (!isSaved_5) {
+      setPaletteColor_1('#'+Math.floor(Math.random()*16777215).toString(16))
+    };
+  }
 
   return (
     <main className='app'>
--- src/ColorContainer.js
+++ src/ColorContainer.js
@@ -6,9 +6,9 @@ const ColorContainer = ({paletteColors, isSavedStatuses, setIsSavedFunctions}) =
   let {paletteColor_1, paletteColor_2, paletteColor_3, paletteColor_4, paletteColor_5} = paletteColors;
   let {isSaved_1, isSaved_2, isSaved_3, isSaved_4, isSaved_5} = isSavedStatuses;
   let {setIsSaved_1, setIsSaved_2, setIsSaved_3, setIsSaved_4, setIsSaved_5} = setIsSavedFunctions;
-  let paletteColors = [paletteColor_1, paletteColor_2, paletteColor_3, paletteColor_4, paletteColor_5];
+  let colors = [paletteColor_1, paletteColor_2, paletteColor_3, paletteColor_4, paletteColor_5];
 
-  const displayColorCards = paletteColors.map((color, index) => {
+  const displayColorCards = colors.map((color, index) => {
     return (
       <ColorCard
         hexCode={color}
