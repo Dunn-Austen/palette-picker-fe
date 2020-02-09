f6d9037d63902bd40e1443a803ffdbeac4b364c7
Alan Turing <austeyd@gmail.com>
2020-02-09 11:30:34 -0700

    Fix hexCode typo

--- src/Components/ColorContainer/ColorContainer.js
+++ src/Components/ColorContainer/ColorContainer.js
@@ -1,18 +1,29 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import ColorCard from '../ColorCard/ColorCard';
 import './ColorContainer.css';
 
 const ColorContainer = ({paletteColors, isSavedStatuses, setIsSavedFunctions}) => {
-  let {paletteColor_1, paletteColor_2, paletteColor_3, paletteColor_4, paletteColor_5} = paletteColors;
-  let {isSaved_1, isSaved_2, isSaved_3, isSaved_4, isSaved_5} = isSavedStatuses;
-  let {setIsSaved_1, setIsSaved_2, setIsSaved_3, setIsSaved_4, setIsSaved_5} = setIsSavedFunctions;
-  let colors = [paletteColor_1, paletteColor_2, paletteColor_3, paletteColor_4, paletteColor_5];
+  let { paletteColor_1, paletteColor_2, paletteColor_3, paletteColor_4, paletteColor_5 } = paletteColors;
+  let { isSaved_1, isSaved_2, isSaved_3, isSaved_4, isSaved_5 } = isSavedStatuses;
+  let { setIsSaved_1, setIsSaved_2, setIsSaved_3, setIsSaved_4, setIsSaved_5 } = setIsSavedFunctions;
+  let combinedColorData =
+    [
+      {hexCode: paletteColor_1, isSaved: isSaved_1, setIsSaved: setIsSaved_1},
+      {hexCode: paletteColor_2, isSaved: isSaved_2, setIsSaved: setIsSaved_2},
+      {hexCode: paletteColor_3, isSaved: isSaved_3, setIsSaved: setIsSaved_3},
+      {hexCode: paletteColor_4, isSaved: isSaved_4, setIsSaved: setIsSaved_4},
+      {hexCode: paletteColor_5, isSaved: isSaved_5, setIsSaved: setIsSaved_5},
+    ];
 
-  const displayColorCards = colors.map((color, index) => {
+    console.log(combinedColorData);
+
+  const displayColorCards = combinedColorData.map((colorData, index) => {
     return (
       <ColorCard
-        hexCode={color}
-        key={color}
+        hexCode={colorData.hexCode}
+        isSaved={colorData.isSaved}
+        setIsSaved={colorData.setIsSaved}
+        key={index}
       />
     );
   });
