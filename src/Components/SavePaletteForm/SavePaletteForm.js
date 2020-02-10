ccb6c8171967a14a24beeaa1426ef5ddc6f480e7
Alan Turing <austeyd@gmail.com>
2020-02-10 13:53:15 -0700

    Establish SavePaletteForm base code

--- src/Components/SavePaletteForm/SavePaletteForm.js
+++ src/Components/SavePaletteForm/SavePaletteForm.js
@@ -1,1 +1,14 @@
 //Do I set this up as a class component and avoid hooks?
+import React, {useState, useEffect} from 'react';
+import './SavePaletteForm.css';
+
+const SavePaletteForm = () => {
+
+  return (
+    <section className='palette-form'>
+
+    </section>
+  );
+}
+
+export default SavePaletteForm;
