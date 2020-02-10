a1f86cf6deff1c38a59cbe6a33c63ea8e4c09d99
Dustin Mikusko <dustinjm713@yahoo.com>
2020-02-10 12:32:04 -0700

    Move apiCalls test code to correct folder

--- apiCalls.test.js
+++ apiCalls.test.js
@@ -1,3 +1,36 @@
 import { fetchAllProjects, fetchSingleProject, postNewProject, patchProject, patchPalette, deletePalette, fetchSinglePalette, fetchAllPalettes, postPalette, deleteProject } from './apiCalls';
 
+descrive('apiCalls', () => {
+   
+  describe('fetchAllProjects', () => {
+    let mockResponse
+
+    beforeEach(() => {
+      mockResponse = {
+        projects: [
+          {
+            id: 1, 
+            title: "Kitchen"
+          }, 
+          {
+            id: 2, 
+            title: "Bedroom"
+          }
+        ]
+      };
+      window.fetch = jest.fn().mockImplementaion(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(mockResopnse)
+        });
+      });
+    });
+
+    it('should call fetch with the correct URL', () => {
+      fetchAllProjects();
+
+      expect(window.fetch).toHaveBeenCalledWith('https://palette-picker-1908.herokuapp.com/api/v1/projects');
+    })
+  })
+})
 
