d8c5a65d54a8d30a7f414e543940adb14aeaef65
Alan Turing <austeyd@gmail.com>
2020-02-11 16:04:41 -0700

    Add project files

---
+++ src/Components/ProjectContainer/ProjectContainer.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react';
+import './ProjectContainer.css';
+import ProjectContainer from 'prop-types';
+
+
+const ProjectContainer = () => {
+
+  return (
+    <section className='form-section'>
+    </section>
+  );
+}
+
+export default SavePaletteForm;
