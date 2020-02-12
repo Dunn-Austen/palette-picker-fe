2eded3935acd8c1adb7c869e5c6c87da2f9e7fc6
Alan Turing <austeyd@gmail.com>
2020-02-11 17:14:59 -0700

    Add PaletteCard files

---
+++ src/Components/PaletteCard/PaletteCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './PaletteCard.css';
+import PropTypes from 'prop-types';
+
+
+const PaletteCard = () => {
+
+  return (
+    <section className='palette-card'>
+
+    </section>
+  )
+}
+
+export default PaletteCard;
