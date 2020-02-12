7044f0f8fc7f611c0a3ff29019a82669b09d7a27
Alan Turing <austeyd@gmail.com>
2020-02-11 17:56:34 -0700

    Build out ProjectCard

---
+++ src/Components/ProjectCard/ProjectCard.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './ProjectCard.css';
+import PropTypes from 'prop-types';
+import PaletteCard from '../PaletteCard/PaletteCard'
+
+const ProjectCard = ({title, palettes}) => {
+
+  const displayPaletteCards = palettes.map((palette, index) => {
+    return (
+      <PaletteCard
+        title={palette.title}
+        project_id={palette.project_id}
+        color_1_id={palette.color_1_id}
+        color_2_id={palette.color_2_id}
+        color_3_id={palette.color_3_id}
+        color_4_id={palette.color_4_id}
+        color_5_id={palette.color_5_id}
+        key={index}
+      />
+    )
+  });
+
+  return (
+    <section className='project-card'>
+      {displayPaletteCards}
+    </section>
+  )
+}
+
+export default ProjectCard;
