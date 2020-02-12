241bbaa7fd35a98fb4b4e388b4a23be6350734c7
Alan Turing <austeyd@gmail.com>
2020-02-11 17:56:43 -0700

    Build out PaletteCard

--- src/Components/PaletteCard/PaletteCard.js
+++ src/Components/PaletteCard/PaletteCard.js
@@ -2,12 +2,26 @@ import React from 'react';
 import './PaletteCard.css';
 import PropTypes from 'prop-types';
 
-
-const PaletteCard = () => {
+const PaletteCard = ({title, project_id, color_1_id, color_2_id, color_3_id, color_4_id, color_5_id}) => {
 
   return (
     <section className='palette-card'>
-
+      <section className='palette-container'>
+        <div className='palette-slice' style={{backgroundColor: color_1_id}}>
+        </div>
+        <div className='palette-slice' style={{backgroundColor: color_2_id}}>
+        </div>
+        <div className='palette-slice' style={{backgroundColor: color_3_id}}>
+        </div>
+        <div className='palette-slice' style={{backgroundColor: color_4_id}}>
+        </div>
+        <div className='palette-slice' style={{backgroundColor: color_5_id}}>
+        </div>
+      </section>
+      <div className='btn-container'>
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
     </section>
   )
 }
